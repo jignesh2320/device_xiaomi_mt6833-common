3e4f776454eb65b774198e656a21df12d7e65042
Alcatraz323 <alcatraz32323@gmail.com>
2024-12-12 21:07:07 +0800

    mt6833-common: Implement VolumeSynchronizer
    * This is needed to synchronize volume with framework
      to limit bass when the volume is high to prevent
      distortion.

--- mt6833.mk
+++ mt6833.mk
@@ -69,6 +69,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     MtkInCallService
 
+PRODUCT_PACKAGES += \
+    VolumeSynchronizer
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_device.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_device.xml \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
