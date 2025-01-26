053f186d2c74ecde9b2e912c83f58cbeefc5a415
Buildbot_Jignesh <jigneshshukla212@gmail.com>
2025-01-26 22:40:55 +0530

    mt6833-common: Switch to common lights HAL

--- mt6833.mk
+++ mt6833.mk
@@ -219,7 +219,7 @@ PRODUCT_PACKAGES += \
 
 # Light
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
+    android.hardware.light-service.lineage
 
 # Linker
 PRODUCT_COPY_FILES += \
